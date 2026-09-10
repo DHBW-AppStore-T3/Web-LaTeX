452dee9bae7fe3ce2c29ac54378681d2a332ac32
Max(imal gutaussehend) <illuminati_max@web.de>
2026-09-10 17:22:02 +0200

    fix(terraform): remove hardcoded project-specific UUIDs for network and security group

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -37,7 +37,6 @@ variable "assignment_files" {
 variable "network_uuid" {
   description = "UUID des internen Netzwerks @openstack:network:id"
   type        = string
-  default     = "34a00b87-57ce-42c4-8e1b-9ea8a657ec2e"
 }
 
 variable "floating_ip_pool" {
@@ -49,5 +48,4 @@ variable "floating_ip_pool" {
 variable "shared_secgroup_id" {
   description = "ID der gemeinsamen Security Group @openstack:security_group:id"
   type        = string
-  default     = "4ffaf007-df66-4250-9118-1bd99378d34a"
 }
