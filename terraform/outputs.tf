67ef15d08c2aef1be5634b0516ea450efa8d5ed2
Max(imal gutaussehend) <illuminati_max@web.de>
2026-09-10 18:25:45 +0200

    fix: use team_ip (IPv6) in outputs.tf instead of hardcoded fixed_ip_v4

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,7 +6,7 @@
 # local.user_accounts = {
 #   "<team>-<username>": {
 #     type     = "password"
-#     ip       = "1.2.3.4"
+#     ip       = "2001:7c0:..."
 #     port     = 80
 #     username = "team-a@example.com"
 #     auth     = "<password>"
@@ -29,9 +29,7 @@ output "team_vms" {
     for team in local.teams_list : team => {
       instance_id   = openstack_compute_instance_v2.team_vm[team].id
       instance_name = openstack_compute_instance_v2.team_vm[team].name
-      fixed_ip      = openstack_compute_instance_v2.team_vm[team].network[0].fixed_ip_v4
-      floating_ip   = local.enable_floating_ip ? openstack_networking_floatingip_v2.team_fip[team].address : null
-      url           = local.enable_floating_ip ? "http://${openstack_networking_floatingip_v2.team_fip[team].address}" : "http://${openstack_compute_instance_v2.team_vm[team].network[0].fixed_ip_v4}"
+      ip            = local.team_ip[team]
     }
   }
 }
